bc14df1982cfae2cd7cf9191ed8d3b483bd4e3f7
Julien Cohen-Adad <jcohen@polymtl.ca>
2021-04-15 15:56:33 -0400

    Updated setup.py: Added URL and email address

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='assign-reviewers',
-    version='0.1',
+    version='0.1.4',
     author='Julien Cohen-Adad',
-    # author_email='aac@example.com',
+    author_email='jcohen@polymtl.ca',
     packages=find_packages(),
-    url='',
+    url='https://github.com/jcohenadad/assign-reviewers',
     license='LICENSE',
     description='Assign reviewers and create scoring excel sheets.',
     long_description=open('README.md').read(),
